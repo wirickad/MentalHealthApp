f81886f36843ed85846fc0421770fc08dfeffbe1
wirickad <wirickad@gmail.com>
2023-12-10 11:27:53 -0700

    Merged branches

--- screens/Get Help/TalkToTherapist.js
+++ screens/Get Help/TalkToTherapist.js
@@ -133,7 +133,7 @@ const styles = StyleSheet.create({
     paddingVertical: 0,
   },
   messageContainer: {
-    maxWidth: "50%",
+    maxWidth: "80%",
     borderRadius: 8,
     padding: 10,
     marginBottom: 10,
